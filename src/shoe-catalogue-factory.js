680fce9509b760171ca1a1fbc60db4261876bcd2
Hidaayat <hidaayat07@gmail.com>
2023-07-21 16:14:31 +0200

    initial commit

---
+++ src/shoe-catalogue-factory.js
@@ -0,0 +1,166 @@
+function ShoeCatalogue() {
+    const shoes = [
+        {
+            name: "Scott formal shoe",
+            brand: "GINO PAOLI",
+            colour: "Grey",
+            size: 6,
+            price: 799,
+            in_stock: 5
+        },
+        {
+            name: "Nanni 38 loafer",
+            brand: "MAZERATA",
+            colour: "Tan",
+            size: 8,
+            price: 559,
+            in_stock: 5
+        },
+        {
+            name: "Miraylle",
+            brand: "ALDO",
+            colour: "Brown",
+            size: 10,
+            price: 2199,
+            in_stock: 5
+        },
+        {
+            name: "Percival",
+            brand: "ALDO",
+            colour: "Burgundy",
+            size: 7,
+            price: 1649,
+            in_stock: 5
+        },
+        {
+            name: "Nanni 36 - loafer",
+            brand: "MAZERATA",
+            colour: "Black",
+            size: 6,
+            price: 664,
+            in_stock: 5
+        },
+        {
+            name: "Magio 72 formal",
+            brand: "MAZERATA",
+            colour: "Dark Brown",
+            size: 8,
+            price: 559,
+            in_stock: 5
+        },
+        {
+            name: "Monetto",
+            brand: "ALDO",
+            colour: "Grey",
+            size: 9,
+            price: 1649,
+            in_stock: 5
+        },
+        {
+            name: "Sergio formal shoe",
+            brand: "GINO PAOLI",
+            colour: "Brown",
+            size: 7,
+            price: 799,
+            in_stock: 5
+        },
+        {
+            name: "Killyon",
+            brand: "CALL IT SPRING",
+            colour: "Brown",
+            size: 8,
+            price: 899,
+            in_stock: 5
+        },
+        {
+            name: "Albeck",
+            brand: "ALDO",
+            colour: "Tan",
+            size: 6,
+            price: 1899,
+            in_stock: 5
+        },
+        {
+            name: "Magio 52 loafer",
+            brand: "MAZERATA",
+            colour: "Dark Brown",
+            size: 7,
+            price: 551,
+            in_stock: 5
+        },
+        {
+            name: "Wingstroll",
+            brand: "ALDO",
+            colour: "Black",
+            size: 9,
+            price: 1699,
+            in_stock: 5
+        },
+        {
+            name: "Matey leather",
+            brand: "STEVE MADDEN",
+            colour: "Black",
+            size: 6,
+            price: 250,
+            in_stock: 5
+        },
+        {
+            name: "Grazie 61 canvas",
+            brand: "MAZERATA",
+            colour: "Grey",
+            size: 8,
+            price: 899,
+            in_stock: 5
+        },
+        {
+            name: "Ybiari",
+            brand: "CALL IT SPRING",
+            colour: "Tan",
+            size: 10,
+            price: 899,
+            in_stock: 5
+        },
+        {
+            name: "Oxford leather lace up",
+            brand: "POLO",
+            colour: "Burgundy",
+            size: 8,
+            price: 2999,
+            in_stock: 5
+        },
+        {
+            name: "Brogue toe cap gibson leather lace up",
+            brand: "POLO",
+            colour: "Brown",
+            size: 7,
+            price: 2099,
+            in_stock: 5
+        },
+        {
+            name: "black",
+            brand: "gucci",
+            colour: "",
+            size: 6,
+            price: 250,
+            in_stock: 5
+        },
+        {
+            name: "black",
+            brand: "gucci",
+            colour: "",
+            size: 9,
+            price: 250,
+            in_stock: 5
+        },
+        {
+            name: "Burnished toe cap gibson leather lace up",
+            brand: "POLO",
+            colour: "Dark Brown",
+            size: 9,
+            price: 2999,
+            in_stock: 5
+        }
+    ]
+
+    return shoes;
+}
