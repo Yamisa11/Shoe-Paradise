85e056459bcb55d2c4bc755f346cbafcca3ffbce
Hidaayat <hidaayat07@gmail.com>
2023-10-03 15:56:13 +0200

    added shopping cart icon and badge

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const brandSelect = document.querySelector("#brandSelect");
 const colourSelect = document.querySelector("#colourSelect");
 const sizeSelect = document.querySelector("#sizeSelect");
 const showAllBtn = document.querySelector("#showAllBtn");
+const cartIconBadge = document.querySelector(".cart-icon-badge");
 
 var slideshows = document.querySelectorAll('[data-component="slideshow"]')
 
@@ -13,28 +14,28 @@ function initSlideShow(slideshow) {
     var slides = document.querySelectorAll(`.slide`); // Get an array of slides
 
     var index = 0, time = 5000;
-    slides[index].classList.add('active');  
-    
-    setInterval( () => {
-      slides[index].classList.remove('active');
-      
-      //Go over each slide incrementing the index
-      index++;
-      
-      // If you go over all slides, restart the index to show the first slide and start again
-      if (index === slides.length) index = 0; 
-      
-      slides[index].classList.add('active');
+    slides[index].classList.add('active');
+
+    setInterval(() => {
+        slides[index].classList.remove('active');
+
+        //Go over each slide incrementing the index
+        index++;
+
+        // If you go over all slides, restart the index to show the first slide and start again
+        if (index === slides.length) index = 0;
+
+        slides[index].classList.add('active');
 
     }, time);
-  }
+}
 
 const shoeCatalogue = ShoeCatalogue();
 
 function displayShoes(arr) {
 
     arr.forEach(item => {
-    
+
         const shoeContainer = document.createElement("div");
         const shoeImg = document.createElement("img");
         const shoeName = document.createElement("p");
@@ -43,7 +44,7 @@ function displayShoes(arr) {
         const shoeStock = document.createElement("p");
         const addToCartBtn = document.createElement("button");
         const centeredBtnContainer = document.createElement("div");
-    
+
         shoeImg.src = item.img_src;
         shoeName.innerText = item.name;
         shoeBrand.innerText = item.brand;
@@ -55,35 +56,44 @@ function displayShoes(arr) {
         shoePrice.style.color = "#333535";
         addToCartBtn.style.border = "1px solid #333535";
         shoeStock.style.color = "#048c44";
-    
+
         shoeContainer.className = "shoeContainer";
         shoeImg.className = "shoeImage";
         shoeStock.className = "shoeStock";
         addToCartBtn.className = "addToCartBtn";
         centeredBtnContainer.className = "centeredBtnContainer"
-    
+
         centeredBtnContainer.append(addToCartBtn);
         shoeContainer.append(shoeImg, shoeName, shoeBrand, shoePrice, shoeStock, centeredBtnContainer);
         shoesDisplay.append(shoeContainer)
     })
 
     const addBtnElem = document.querySelectorAll(".addToCartBtn");
-    
+
     addBtnElem.forEach(item => {
-    
+
         item.addEventListener("click", () => {
-    
+
             const shoeName = item.closest(".shoeContainer").children[1].innerText;
-    
+
             shoeCatalogue.addToCart(shoeName)
-    
+
             const shoeItem = item.closest(".shoeContainer").children[4];
-    
+
+            let cartItemsAdded = Number(cartIconBadge.innerText)
+
+            if (Number(shoeItem.innerText[0]) !== 0) {
+                cartItemsAdded++;
+            }
+
+            cartIconBadge.innerText = cartItemsAdded;
+
             shoeCatalogue.shoes.forEach(item => {
-                if(shoeName === item.name) {
+                if (shoeName === item.name) {
                     shoeItem.innerHTML = `<span>${item.in_stock} </span>in stock`;
                 }
             })
+
         })
     })
 }
@@ -120,3 +130,4 @@ showAllBtn.addEventListener("click", () => {
     colourSelect.value = "default";
     sizeSelect.value = "default";
 })
+
