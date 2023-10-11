2009691f70983b60f10c3313cf501db81992e53e
Hidaayat Williams <hidaayat07@gmail.com>
2023-10-11 14:01:39 +0200

    added page for adding shoe

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { engine } from "express-handlebars";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.engine("handlebars", engine({
+    layoutsDir: "./views/layouts"
+}));
+
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/add", (req, res) => {
+    res.render("addShoe")
+})
+
+app.listen(PORT, () => console.log(`Server started at Port: ${PORT}`));
